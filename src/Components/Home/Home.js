1fd5ac7bb82906222bfde0d0b2b4d6904b729176
SaiSrinivas-10 <saikeesari2000@gmail.com>
2021-07-17 23:46:13 +0530

    changes frontend

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,31 +1,51 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Movie from "../Movies/Movie";
+import { saveAs } from "file-saver";
+import { Container } from "react-bootstrap";
 // import Dropdownbox from "../GlobalComponents/Dropdown";
 
 export default function Home() {
-  // const MoviesList = useRef([]);
-  const [MoviesList, setMoviesList] = useState([1, 2, 3, 4]);
-  // useEffect(async () => {
-  //   const temp = await axios
-  //     .get("http://localhost:3000/getmovies")
-  //     .then((res) => {
-  //       setMoviesList(res.data);
-  //       console.log(MoviesList);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
+  const [MoviesList, setMoviesList] = useState([]);
+  const [Id, setId] = useState("");
+  console.log(Id);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/getmovies")
+      .then((res) => {
+        setMoviesList(res.data);
+        console.log(MoviesList);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  const movies = () => {
-    var temp = [1, 2, 3, 4];
-    temp.map((m) => {
-      return <Movie />;
-    });
+  const playvideo = () => {
+    axios
+      .post("http://localhost:3000/getmovie", { id: Id })
+      .then((res) => {
+        var file = res.data;
+        saveAs(res);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
+
+  const movies = () =>
+    MoviesList.map((moviedata, i) => {
+      return (
+        <div key={i++}>
+          <Movie playvideo={playvideo} setId={setId} data={moviedata} />
+        </div>
+      );
+    });
+
   return (
-    <div>
-      <Movie />
-      {movies()}
-    </div>
+    <Container className="mt-3">
+      <div className="d-flex flex-wrap justify-content-around">
+        {movies()}
+        {movies()}
+        {movies()}
+      </div>
+    </Container>
   );
 }
--- src/Components/Movies/Movie.js
+++ src/Components/Movies/Movie.js
@@ -1,15 +1,31 @@
-import react from "react";
-
 export default function Movie(props) {
-  console.log(props);
   return (
-    <div>
-      hi
+    <div
+      className="mt-3"
+      style={{ cursor: "pointer" }}
+      onClick={() => {
+        props.setId(props.data.id);
+        props.playvideo();
+      }}
+    >
       <img
+        style={{ width: "200px", height: "200px", objectFit: "cover" }}
         id="base64image"
-        src={`data:image/jpeg;base64,${props.image}`}
-        alt="Missing"
+        src={`data:image/jpeg;base64,${props.data.thumbnail}`}
+        alt="Thumbnail"
       />
+      <div
+        style={{
+          backgroundColor: "black",
+          width: "100%",
+          padding: "5px",
+        }}
+      >
+        <div style={{ color: "white", fontWeight: "bold" }}>
+          {props.data.name}
+        </div>
+        <div style={{ color: "grey" }}>{props.data.year}</div>
+      </div>
     </div>
   );
 }
